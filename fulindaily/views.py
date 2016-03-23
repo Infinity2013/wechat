b5dd1c5fc6329ed958efff606f0ca413aed5f18a
wxl <wxl@wxls-MacBook-Air.local>
2016-03-23 19:05:09 +0800

    fulindaily wechat public platform init

---
+++ fulindaily/views.py
@@ -0,0 +1,31 @@
+import hashlib
+
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+WECHAT_TOKEN = "weixinpublicplatform2016"
+
+
+@csrf_exempt
+def index(request):
+    if request.method == 'GET':
+        response = HttpResponse(checkSignature(request))
+        return response
+    else:
+        return HttpResponse('Hello World')
+
+
+def checkSignature(request):
+    signature = request.GET.get('signature', None)
+    timestamp = request.GET.get('timestamp', None)
+    nonce = request.GET.get('nonce', None)
+    echostr = request.GET.get('echostr', None)
+    token = WECHAT_TOKEN
+    tmplist = [token, timestamp, nonce]
+    tmplist.sort()
+    tmpstr = "%s%s%s" % tuple(tmplist)
+    tmpstr = hashlib.sha1(tmpstr).hexdigest()
+    if tmpstr == signature:
+        return echostr
+    else:
+        return None
